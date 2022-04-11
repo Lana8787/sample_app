737d0ec1ed9c4ac718e31c93c13f82f09f2fafc3
Lana8787 <yulyamc87@gmail.com>
2022-04-11 23:41:25 +0300

    Added log in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,40 @@
 class User < ApplicationRecord
+    attr_accessor :remember_token
     before_save { email.downcase! }
     validates(:name, presence: true, length: { maximum: 50 })
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates(:email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false })
     has_secure_password
     validates :password, length: { minimum: 6 }
-    
+
+    # Возвращает дайджест для указанной строки.
+    def User.digest(string)
+        cost = ActiveModel::SecurePassword.min_cost ?
+        BCrypt::Engine::MIN_COST :
+        BCrypt::Engine.cost
+        BCrypt::Password.create(string, cost: cost)
+    end
+
+    #Возвращает случайний токен
+    def User.new_token
+        SecureRandom.urlsafe_base64
+    end
+
+    #Запоминает пользователя в базе данных для использования в постоянных сеансах
+    def remember
+        self.remember_token = User.new_token
+        update_attribute(:remember_digest, User.digest(remember_token))
+    end
+
+    #Возвращает true, если указанний токен соответствует дайджесту
+    def authenticated?(attribute, token)
+        digest = send("#{attribute}_digest")
+        return false if digest.nil?
+        BCrypt::Password.new(digest).is_password?(token)
+      end
+
+    #Забыть пользователя
+    def forget
+        update_attribute(:remember_digest, nil)
+    end
 end
